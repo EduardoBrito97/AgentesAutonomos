77e13551f63d5451d1708b9cd8379285a65b1afc
Eduardo Barreto <eduardobarreto97@gmail.com>
2021-04-15 21:24:18 -0300

    Fix attack command and improve build order

--- ProtossBot.py
+++ ProtossBot.py
@@ -40,23 +40,29 @@ class ProtossBot(sc2.BotAI):
 
         nexus = self.units(NEXUS).ready.random
 
-        # 16 probes por Nexus
+        # 16 Probes por Nexus
         if self.workers.amount < self.units(NEXUS).amount*16 and nexus.is_idle and self.can_afford(PROBE):
             await self.do(nexus.train(PROBE))
 
-        # Precisamos de ao menos 1 Pylon
+        # Precisamos de ao menos 1 Pylon, mas também precisamos de Pylons sempre que não tivermos supplies
         should_create_pylon = self.units(PYLON).amount == 0 or self.supply_left < 2
-        # Mas também precisamos de Pylons sempre que não tivermos supplies
+        
+        amount_of_structures = self.units(GATEWAY).amount + self.units(CYBERNETICSCORE).amount
+
+        # Precisamos de 1 Pylon pra cada estrutura tbm
+        should_create_pylon = should_create_pylon or self.units(PYLON).amount <= amount_of_structures
+
+        # Por fim, analisamos se é possível construir um Pylon (ou se já temos algum em construção)
         should_create_pylon = should_create_pylon and not self.already_pending(PYLON) and self.can_afford(PYLON)
         
         if should_create_pylon:
             await self.build(PYLON, near = nexus)
 
         # Nosso foco é o Gateway, então precisamos construir pelo menos dois
-        if self.units(GATEWAY).amount < 3 and self.units(PYLON).ready.amount > 0:
+        if self.units(GATEWAY).amount < 3 and self.units(PYLON).ready.amount > 0 and not self.already_pending(GATEWAY):
             pylon = self.units(PYLON).ready.random
             if pylon and self.can_afford(GATEWAY):
-                await self.build(GATEWAY, near=pylon)
+                await self.build(GATEWAY, near = pylon)
         
         # Em seguida, partimos para criar um Cybernetics Core
         if self.units(GATEWAY).ready.amount >= 2 and not self.units(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
@@ -65,7 +71,7 @@ class ProtossBot(sc2.BotAI):
                 await self.build(CYBERNETICSCORE, near = pylon)
 
         # Para criar os Stalkers
-        elif self.units(CYBERNETICSCORE).ready.exists and self.can_afford(RESEARCH_WARPGATE) and not self.warpgate_started:
+        if self.units(CYBERNETICSCORE).ready.exists and self.can_afford(RESEARCH_WARPGATE) and not self.warpgate_started:
             ccore = self.units(CYBERNETICSCORE).ready.first
             await self.do(ccore(RESEARCH_WARPGATE))
             self.warpgate_started = True
@@ -77,7 +83,7 @@ class ProtossBot(sc2.BotAI):
                    await self.do(gateway.train(ZEALOT))
 
         # Por fim, criamos os Stalkers
-        if self.units(GATEWAY).ready.amount >= 2 and self.units(STALKER).ready.amount < 5:
+        if self.units(GATEWAY).ready.amount >= 2 and self.units(STALKER).ready.amount < 5 and self.warpgate_started:
             for gateway in self.units(GATEWAY).ready:
                 if self.can_afford(STALKER) and gateway.is_idle:
                    await self.do(gateway.train(STALKER))
@@ -86,11 +92,23 @@ class ProtossBot(sc2.BotAI):
         
         # Atacando
         if army_is_ready:
-            for vr in self.units(ZEALOT).ready.idle:
-                await self.do(vr.attack(self.enemy_start_locations[0]))
-            
-            for vr in self.units(STALKER).ready.idle:
-                await self.do(vr.attack(self.enemy_start_locations[0]))
+            for zealot in self.units(ZEALOT).ready:
+                has_targets = self.cached_known_enemy_units or self.cached_known_enemy_structures
+                if has_targets:
+                    targets = (self.cached_known_enemy_units | self.cached_known_enemy_structures).filter(lambda unit: unit.can_be_attacked)
+                    target = targets.closest_to(zealot)
+                    await self.do(zealot.attack(target))
+                else:
+                    await self.do(zealot.attack(self.enemy_start_locations[0]))
+
+            for stalker in self.units(STALKER).ready.idle:
+                has_targets = self.cached_known_enemy_units or self.cached_known_enemy_structures
+                if has_targets:
+                    targets = (self.cached_known_enemy_units | self.cached_known_enemy_structures).filter(lambda unit: unit.can_be_attacked)
+                    target = targets.closest_to(stalker)
+                    await self.do(stalker.attack(target))
+                else:
+                    await self.do(stalker.attack(self.enemy_start_locations[0]))
 
 
 def main():
